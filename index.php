fda8323eebb9801bd13077fb13bdfc41c7fb96c0
Onkar Dighe <onkardigheofficial@gmail.com>
2021-08-16 10:23:11 +0530

    Final

--- index.php
+++ index.php
@@ -186,11 +186,11 @@
                             <li><i class="bi bi-check-circle"></i> 2. Becoming certified shows you’ve got genuine digital skills, and that you are motivated to learn.</li>
                             <li><i class="bi bi-check-circle"></i> 3. Improving your digital knowledge can help you find a job,get promoted, or start a whole new business.</li>
                         </ul>
-                        <p>
+                        <!-- <p>
                             Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
                             velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
                             culpa qui officia deserunt mollit anim id est laborum
-                        </p>
+                        </p> -->
                     </div>
                 </div>
 
@@ -206,49 +206,26 @@
         <div class="row">
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
             <div class="icon-box">
-              <div class="icon"><i class="bx bxl-dribbble"></i></div>
-              <h4><a href="">Lorem Ipsum</a></h4>
-              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+              <!-- <div class="icon"><i class="bx bxl-dribbble"></i></div> -->
+              <div class="icon"><img src="img/marketing.png" class="img-fluid" alt=""></i></div>
+              <h4><a href="">Marketing</a></h4>
+              <p>SEO Optimisation, Social Media Marketing, Email Marketing, Content Markrting</p>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
             <div class="icon-box">
-              <div class="icon"><i class="bx bx-file"></i></div>
-              <h4><a href="">Sed ut perspiciatis</a></h4>
-              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+            <div class="icon"><img src="img/bussiness.png" class="img-fluid" alt=""></i></div>
+              <h4><a href="">Bussiness</a></h4>
+              <p>Recruitements, Industries, MLM, Bussiness</p>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
             <div class="icon-box">
-              <div class="icon"><i class="bx bx-tachometer"></i></div>
-              <h4><a href="">Magni Dolores</a></h4>
-              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-            <div class="icon-box">
-              <div class="icon"><i class="bx bx-world"></i></div>
-              <h4><a href="">Nemo Enim</a></h4>
-              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-            <div class="icon-box">
-              <div class="icon"><i class="bx bx-slideshow"></i></div>
-              <h4><a href="">Dele cardo</a></h4>
-              <p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-            <div class="icon-box">
-              <div class="icon"><i class="bx bx-arch"></i></div>
-              <h4><a href="">Divera don</a></h4>
-              <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
+            <div class="icon"><img src="img/personalDev.png" class="img-fluid" alt=""></i></div>
+              <h4><a href="">Personal Development</a></h4>
+              <p>Communicatin, Interview, Public-Speaking, Soft Skills, Body language, Negotiation</p>
             </div>
           </div>
 
