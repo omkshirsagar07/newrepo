6b3b5560af6ecbe3d32e64300d38bf4ae3c096b9
Onkar Dighe <onkardigheofficial@gmail.com>
2021-08-23 20:00:42 +0530

    aboutus files created

---
+++ aboutus copy.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get Success Today</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="css/home.css">
+
+</head>
+
+<body class="mobile">
+    <nav class="navigation-pane">
+        <div class="container nav-wrapper">
+            <div class="brand" onclick="location.href='index.php'">
+                <div class="brand-img">
+                    <img src="img/logo.png" alt="">
+                </div>
+                <span><strong>GET SUCCESS TODAY</strong></span>
+            </div>
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul class="nav-list">
+                <li class="active">
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a class="nav-services" href="#">Services </a>
+                    <ul class="dropdown-list">
+                        <li><a href="#">SEO optimisation</a></li>
+                        <li><a href="#">Digital marketing</a></li>
+                        <li><a href="#">Web development</a></li>
+                        <li><a href="#">Web design</a></li>
+                        <li><a href="#">Software development</a></li>
+                        <li><a href="#">Branding</a></li>
+                        <li><a href="#">Content writing</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Q&A</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li>
+                    <button class="btn">Login</button>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <main>
+        <section class="header container">
+            <div>
+                <h1>About Us</h1>
+                <h4>Lorem ipsum dolor sit amet consectetur.</h4>
+                <div>
+                    <button class="btn">Watch video now</button>
+                </div>
+            </div>
+        </section>
+    </main>
+    <div class="sticky-container" id="sticky-container">
+        <ul class="sticky">
+            <!-- <li onclick="socialLinks('fb')">
+                <img width="32" height="32" title="" alt="" src="img/facebook.png" />
+                <p>Facebook</p>
+            </li>
+            <li onclick="socialLinks('twtr')"> 
+                <img width="32" height="32" title="" alt="" src="img/twitter.png" />
+                <p>Twitter</p>
+            </li> -->
+            <li onclick="socialLinks('ig')"> 
+                <img width="32" height="32" title="" alt="" src="img/instagram.png" />
+                <p>Instagram</p>
+            </li>
+            <li onclick="socialLinks('linked')"> 
+                <img width="32" height="32" title="" alt="" src="img/linkedin.png" />
+                <p>Linkedin</p>
+            </li>
+            <li onclick="socialLinks('yt')">
+                <img width="32" height="32" title="" alt="" src="img/youtube.png" />
+                <p>Youtube</p>
+            </li>
+            <li onclick="socialLinks('wp')">
+                <img width="32" height="32" title="" alt="" src="img/whatsapp.png" />
+                <p>WhatsApp</p>
+            </li>
+        </ul>
+    </div>
+    <script src="./js/home.js"></script>
+</body>
+
+</html>
